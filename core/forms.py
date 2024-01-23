020c8e46c394bfade12826ac3a30017949e892a3
Lauri Suuster <89710791+laurisuuster@users.noreply.github.com>
2024-01-23 18:27:53 +1300

    further alterations

--- core/forms.py
+++ core/forms.py
@@ -4,15 +4,57 @@
 from django import forms
 from django.core.validators import EmailValidator
 
-
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField(validators=[EmailValidator()])
-    phone = forms.CharField(max_length=15)
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'block w-full p-2  text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'placeholder': 'Your Name',
+        })
+    )
+    email = forms.EmailField(
+        validators=[EmailValidator()],
+        widget=forms.EmailInput(attrs={
+            'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'placeholder': 'Your Email',
+        })
+    )
+    phone = forms.CharField(
+        max_length=15,
+        widget=forms.TextInput(attrs={
+            'class': 'block w-full p-2  text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'placeholder': 'Your Phone',
+        })
+    )
+    subject = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'block w-full p-2  text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'placeholder': 'Subject',
+        })
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'block w-full p-2  text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'placeholder': 'Your Message',
+            'rows': 4,
+        })
+    )
 
 
 class ReviewForm(forms.Form):
-    stars = forms.IntegerField(max_value=5, min_value=0)
-    comment = forms.CharField(widget=forms.Textarea)
+    stars = forms.IntegerField(
+        max_value=5,
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class': 'block w-full p-2 text-sm text-gray-100 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'placeholder': 'Enter stars (0-5)',
+        })
+    )
+    comment = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'block w-full p-2 text-sm text-gray-100 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'placeholder': 'Enter your review here...',
+            'rows': 4,
+        })
+    )
--- product/forms.py
+++ product/forms.py
@@ -6,34 +6,62 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('first_name', 'last_name', 'address', 'zipcode', 'city', 'shipping_option',)
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                'placeholder': 'Your First Name',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'block w-full p-2  text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                'placeholder': 'Your Last Name',
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                'placeholder': 'Your Address',
+            }),
+            'zipcode': forms.TextInput(attrs={
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                'placeholder': 'Your Zipcode',
+            }),
+            'city': forms.TextInput(attrs={
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                'placeholder': 'Your City',
+            }),
+            'shipping_option': forms.Select(attrs={
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            }),
+        }
 
 # https://medium.com/swlh/how-to-style-your-django-forms-7e8463aae4fa
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('category', 'title', 'description', 'price', 'image','region')
+        fields = ('category', 'title', 'description', 'price', 'image', 'region')
         widgets = {
             'category': forms.Select(attrs={
-                'class': 'w-full p-4 border border-gray-250'
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
             }),
             'title': forms.TextInput(attrs={
-                'class': 'w-full p-4 border border-gray-250'
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                'placeholder': 'Product Title',
             }),
             'description': forms.Textarea(attrs={
-                'class': 'w-full p-4 border border-gray-250'
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                'placeholder': 'Product Description',
             }),
             'price': forms.TextInput(attrs={
-                'class': 'w-full p-4 border border-gray-250'
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                'placeholder': 'Product Price',
             }),
             'image': forms.FileInput(attrs={
-                'class': 'w-full p-4 border border-gray-250'
+                'class': 'block w-full p-2  text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
             }),
             'region': forms.Select(attrs={
-                'class': 'w-full p-4 border border-gray-250'
+                'class': 'block w-full p-2 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
             }),
         }
-
+        
 from .models import ProductReview
 
 class ProductReviewForm(forms.ModelForm):
